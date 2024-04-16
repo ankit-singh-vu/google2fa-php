ce7bf60b4a20da7f4c1b2237a56a36d03bcfb584
ankit-singh-vu <ankisinggggg@gmail.com>
2024-04-16 12:28:08 +0530

    update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "hi";
 require __DIR__ . '/vendor/autoload.php';
 
-use PragmaRX\Google2FAQRCode\Google2FA;
+use PragmaRX\Google2FA\Google2FA;
     
 $google2fa = new Google2FA();
     
@@ -10,13 +10,29 @@ $secretKey = $google2fa->generateSecretKey();
 $companyName='Ankit';
 $companyEmail='ankit.singh@convesio.com';
 
-$inlineUrl = $google2fa->getQRCodeInline(
+$qrCodeUrl = $google2fa->getQRCodeUrl(
     $companyName,
     $companyEmail,
     $secretKey
 );
 
+echo $qrCodeUrl;
+/*
+otpauth://totp/Ankit:ankit.singh%40convesio.com?secret=LAP3IX3PE3CU3W5W&issuer=Ankit&algorithm=SHA1&digits=6&period=30
+*/
 
+$google2fa_url = custom_generate_qrcode_url($companyName,$companyEmail,$secretKey);
+
+function custom_generate_qrcode_url($companyName,$companyEmail,$secretKey){
+    $google2fa = (new \PragmaRX\Google2FAQRCode\Google2FA());
+
+    $inlineUrl = $google2fa->getQRCodeInline(
+        $companyName,
+        $companyEmail,
+        $secretKey
+    );
+    return $inlineUrl;
+}
 ?>
 
-<img src="<?=$inlineUrl?>">
+<?php echo $google2fa_url;?>
